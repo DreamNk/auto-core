1020b85cabb90144a8eb1169e15c6d10ea6ccb3d
Umasankar Tadiparthi <utadiparthi@ghx.com>
2015-11-13 16:57:06 -0700

    added methods for mongo validations

--- src/main/java/com/ghx/auto/core/support/MongoDBClient.java
+++ src/main/java/com/ghx/auto/core/support/MongoDBClient.java
@@ -1,14 +1,20 @@
 package com.ghx.auto.core.support;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.testng.Assert;
 
 import com.ghx.auto.core.ui.support.EnvConfig;
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
@@ -16,7 +22,7 @@ public class MongoDBClient {
 	
 	private EnvConfig envConfig;
 	private String section; 
-	private MongoTemplate mongoTemplate;
+	private MongoOperations mongoOperations;
 	
     public MongoDBClient(EnvConfig envConfig, String section) {
     	this.envConfig = envConfig;
@@ -26,7 +32,7 @@ public class MongoDBClient {
     
     public Set<String> get_collection_names() {
     	try {
-    		return this.mongoTemplate.getCollectionNames();
+    		return this.mongoOperations.getCollectionNames();
 		} catch (MongoException me) {
 			Assert.fail("Unable to get the collection names, error message: " + me.getMessage());
 		} catch (DataAccessException me) {
@@ -36,9 +42,57 @@ public class MongoDBClient {
     	return null;
     }
     
+    public long get_collection_count(String collectionName, String query) {
+    	try {
+    		return mongoOperations.count(new BasicQuery(query), collectionName);
+		} catch (MongoException me) {
+			Assert.fail("Unable to get the collection names, error message: " + me.getMessage());
+		} catch (DataAccessException me) {
+			Assert.fail("Unable to get the collection names, error message: " + me.getMessage());
+		}
+    	
+    	return 0; //should not happen
+    	
+    }
+    
+    public MongoDBClient verify_collection_count(String collectionName, String query, long expectedCount) {
+    	long actualCount = get_collection_count(collectionName, query);
+    	Assert.assertEquals(actualCount, expectedCount);
+    	
+    	return this;
+    }
+    
+    public MongoDBClient verify_collection_count_greater_than_zero(String collectionName, String query) {
+    	long actualCount = get_collection_count(collectionName, query);
+    	Assert.assertTrue(actualCount > 0, "Collection count is not greater than zero, query is: " + query);
+    	
+    	return this;
+    }
+    
+    public MongoDBClient verify_distinct_field_values_from_collection(String collectionName, String fieldName, String queryFilter, Object[] expectedValues) {
+    	Collection actualList = null;
+    	Collection expectedList = Arrays.asList(expectedValues);
+    	Collection diffList;
+    	
+    	try {
+    		actualList = mongoOperations.getCollection(collectionName).distinct("component", BasicDBObject.parse(queryFilter));
+		} catch (MongoException me) {
+			Assert.fail("Unable to verify, error message: " + me.getMessage());
+		} catch (DataAccessException me) {
+			Assert.fail("Unable to verify, error message: " + me.getMessage());
+		}
+    	
+    	diffList = CollectionUtils.disjunction(actualList, expectedList);
+    	
+    	Assert.assertTrue(diffList.size() == 0, "Expected and Actual List values did not match, diff is: " + diffList);
+    	
+    	return this;
+    	
+    }
+    
     private void createMongoTemplate() {
 		try {
-			this.mongoTemplate = new MongoTemplate(new SimpleMongoDbFactory(new MongoClientURI("mongodb://configsvc-user:configsvc123@localhost:27017/configsvc")));
+			this.mongoOperations = new MongoTemplate(new SimpleMongoDbFactory(new MongoClientURI(envConfig.getConfigParamValue(this.section, "uri"))));
 		} catch (UnknownHostException uhe) {
 			Assert.fail("Unknown mongo host, error message: " + uhe.getMessage());
 		}
